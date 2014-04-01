1de1a59ee1a18d83b81ac6b41a9154dd5ea454df
Wells <hsiungfree@gmail.com>
2014-04-01 14:32:09 +0800

    Project MOOCS started now!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Moocs::Application.config.secret_key_base = '5e7dd706faf96975efdd497c782054c77aa3e206a21b8eafab26bef3240663345adebf176234da8904c385a1f90bb5e4461ec9f4b39bdb1d5e350844012e6a24'
